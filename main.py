7d3f46e491d45fb698543ebe0d71a28713ad7554
Adrian Apostol <adrian.apostol@heits.digital>
2024-05-23 16:09:13 +0300

    added description to documents

--- main.py
+++ main.py
@@ -3,7 +3,10 @@ from pathlib import Path
 from dotenv import dotenv_values
 
 from langchain import hub
-from langchain_openai import OpenAIEmbeddings, ChatOpenAI
+from langchain_community.llms import Ollama
+# from langchain_openai import OpenAIEmbeddings, ChatOpenAI
+from langchain_community.embeddings import OllamaEmbeddings
+
 from langchain_chroma import Chroma
 from langchain.docstore.document import Document
 from langchain_core.prompts import ChatPromptTemplate
@@ -18,21 +21,28 @@ from pydantic import BaseModel
 from typing import List
 from PIL import Image
 
+llm = Ollama(model="llama2")
+
 
 class Questions(BaseModel):
     questions: List[str]
 
 
-def update_env_vars(env_file_path: str=None):
+def update_env_vars(env_file_path: str = None):
     if env_file_path:
         env_config = dotenv_values(env_file_path)
         os.environ = {**os.environ, **env_config}
 
+
 class RAG:
     def __init__(self):
-        self.encoder = OpenAIEmbeddings()
-        self.llm = ChatOpenAI(model="gpt-3.5-turbo-0125")
+        # OpenAI
+        # self.encoder = OpenAIEmbeddings()
+        # self.llm = ChatOpenAI(model="gpt-3.5-turbo-0125")
 
+        # Ollama
+        self.encoder = OllamaEmbeddings()
+        self.llm = Ollama(model="llama2")
 
     def query_links(self, links: List[str], query: str) -> str:
         vectorstore = self._get_vectorstore_from_links(links)
@@ -41,15 +51,15 @@ class RAG:
         vectorstore.delete_collection()
         return answer
 
-
     def query_images(self, image_path_list: List[str], query: str) -> str:
-        raw_images = [Image.open(img_path).convert('RGB') for img_path in image_path_list]
-        vectorstore = self._get_vectorstore_from_images(raw_images, image_path_list, query)
+        raw_images = [Image.open(img_path).convert('RGB')
+                      for img_path in image_path_list]
+        vectorstore = self._get_vectorstore_from_images(
+            raw_images, image_path_list, query)
         rag_chain = self.get_rag_chain(vectorstore)
         answer = rag_chain.invoke(query)
         vectorstore.delete_collection()
         return answer
-
 
     def query_youtube_video(self, video_url: str, query: str) -> str:
         vectorstore = self._get_vectorstore_from_youtube(video_url)
@@ -58,20 +68,20 @@ class RAG:
         vectorstore.delete_collection()
         return answer
 
-
     def get_rag_chain(self, vectorstore, top_k=5):
-        retriever = vectorstore.as_retriever(search_type="similarity", search_kwargs={"k": top_k})
+        retriever = vectorstore.as_retriever(
+            search_type="similarity", search_kwargs={"k": top_k})
         prompt = hub.pull("rlm/rag-prompt")
 
         rag_chain = (
-            {"context": retriever | self.format_docs, "question": RunnablePassthrough()}
+            {"context": retriever | self.format_docs,
+                "question": RunnablePassthrough()}
             | prompt
             | self.llm
             | StrOutputParser()
         )
 
         return rag_chain
-
 
     def _get_vectorstore_from_links(self, links):
         # Use this to ask GPT about content on websites
@@ -92,79 +102,126 @@ class RAG:
             )
             docs.extend(loader.load())
 
-        text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=200)
+        text_splitter = RecursiveCharacterTextSplitter(
+            chunk_size=1000, chunk_overlap=200)
         splits = text_splitter.split_documents(docs)
-        vectorstore = Chroma.from_documents(documents=splits, embedding=self.encoder)
+        vectorstore = Chroma.from_documents(
+            documents=splits, embedding=self.encoder)
 
         return vectorstore
-    
 
-    def _get_vectorstore_from_images(self, raw_images_list: List[Image.Image], raw_images_paths: List[str]=None, query: str=""):
+    def _get_vectorstore_from_images(self, raw_images_list: List[Image.Image], raw_images_paths: List[str] = None, query: str = ""):
         # Use this to ask GPT about content in images
         descriptions = self._extract_generic_image_info(raw_images_list)
-        descriptions_questions = self._extract_image_info_based_on_questions(raw_images_list, question=query)
+        # descriptions_questions = self._extract_image_info_based_on_questions(raw_images_list, question=query)
 
         documents = []
-        for i, [desc1, desc2] in enumerate(zip(descriptions, descriptions_questions)):
+        for i, desc in enumerate(descriptions):
             source = raw_images_paths[i] if raw_images_paths != None else f"image_{i}"
-            description = "{" + f"file_name: '{source}', description_generic: '{desc1}', description_specific: '{desc2}'" + "}"
-            doc = Document(page_content=description, metadata={"source": source})
+            pc = "{" + \
+                f"file_name: '{source}', description: '{desc}'" + "}"
+            doc = Document(page_content=pc,
+                           metadata={"source": source})
             documents.append(doc)
         print(documents)
-        
-        vectorstore = Chroma.from_documents(documents=documents, embedding=self.encoder)
-        return vectorstore
 
+        vectorstore = Chroma.from_documents(
+            documents=documents, embedding=self.encoder)
+        return vectorstore
 
     def _get_vectorstore_from_youtube(self, video_url):
         # Use this to ask GPT about content in videos
         transcript = get_transcript_from_url(video_url)
-        
+
         documents = []
         for item in transcript:
             metadata = item.copy()
             page_content = metadata.pop('text', None)
             doc = Document(page_content=page_content, metadata=metadata)
             documents.append(doc)
-        
-        vectorstore = Chroma.from_documents(documents=documents, embedding=self.encoder)
+
+        vectorstore = Chroma.from_documents(
+            documents=documents, embedding=self.encoder)
         return vectorstore
 
     def _get_questions_from_prompt(self, query):
-        parser = PydanticOutputParser(pydantic_object=Questions)
-        formatting = parser.get_format_instructions()
+        # parser = PydanticOutputParser(pydantic_object=Questions)
+        # formatting = parser.get_format_instructions()
 
         prompt = ChatPromptTemplate.from_template(
-            "Pretend you are Sherlock Holmes and you only ask as few questions as possbile to unravel a mistery. What would standalone question would ask about a image to answer the following mistery: {query}. {format_instructions}", 
-            partial_variables={"format_instructions": formatting}
-            )
-        chain = prompt | self.llm | JsonOutputParser()
+            # "Pretend you are Sherlock Holmes and you only ask as few questions as possbile to unravel a mistery. What would standalone question would ask about a image to answer the following mistery: {query}. {format_instructions}",
+            "You will receive a text description of an image. Based on this, provide 3 follow-up questions that can't be answered with yes or no directly, that would allow me to better understand what's in the image. output everything as a simple 1,2,3 list, no other text. description: {query}",
+            # partial_variables={"format_instructions": formatting}
+        )
+        chain = prompt | self.llm
         return chain.invoke(query)
 
+    def _rephrase_question_answer(self, question, answer):
+        prompt = ChatPromptTemplate.from_template(
+            "Rephrase the provided question based on the answer in order to be a statement instead of question. print out just the statement itself, no other text. \nquestion: {query}\nanswer: {answer}",
+            partial_variables={"answer": answer}
+        )
+        chain = prompt | self.llm
+        return chain.invoke(question)
+
     def _extract_generic_image_info(self, raw_images: List, model_type="Salesforce/blip-image-captioning-large"):
-        from transformers import BlipProcessor, BlipForConditionalGeneration
+        from transformers import BlipProcessor, BlipForConditionalGeneration, BlipForQuestionAnswering
 
         descriptions = []
-        
+
         processor = BlipProcessor.from_pretrained(model_type)
         model = BlipForConditionalGeneration.from_pretrained(model_type)
+        model_questioning = BlipForQuestionAnswering.from_pretrained(
+            "Salesforce/blip-vqa-base")
 
         # conditional image captioning
         for image in raw_images:
+            image_descs = []
             conditioning_text = ""
             inputs = processor(image, conditioning_text, return_tensors="pt")
             out = model.generate(**inputs)
             description = processor.decode(out[0], skip_special_tokens=True)
+            print("Image description: ")
             print(description)
-            descriptions.append(description)
-        
+
+            # let's get some more specific questions
+            questions = self._get_questions_from_prompt(description)
+
+            questions_list = questions.split("\n")
+            print("Questions list: ")
+            print(questions_list)
+            replies_list = []
+            # let's go thru the questions and get replies from model
+            for desc in questions_list:
+                if len(desc) > 1 and (desc[0] == '1' or desc[0] == '2' or desc[0] == '3'):
+                    print("Asking: ")
+                    print(desc)
+                    inputs = processor(image, desc, return_tensors="pt")
+                    out = model_questioning.generate(**inputs)
+                    answer = processor.decode(out[0], skip_special_tokens=True)
+                    print("Answer: ")
+                    print(answer)
+
+                    # rephrase
+                    rephrased = self._rephrase_question_answer(desc, answer)
+                    rephrased = rephrased.replace("Statement:", "")
+                    print("rephrased: ")
+                    print(rephrased)
+
+                    replies_list.append(rephrased)
+
+            image_descs.append(description)
+            image_descs.extend(replies_list)
+
+            descriptions.append("\n".join(image_descs))
+
         return descriptions
 
     def _extract_image_info_based_on_questions(self, raw_images: List, question="", model_type="Salesforce/blip-vqa-base"):
         from transformers import BlipProcessor, BlipForQuestionAnswering
 
         descriptions = []
-        
+
         processor = BlipProcessor.from_pretrained(model_type)
         model = BlipForQuestionAnswering.from_pretrained(model_type)
 
@@ -175,14 +232,13 @@ class RAG:
             description = processor.decode(out[0], skip_special_tokens=True)
             print(description)
             descriptions.append(description)
-        
-        return descriptions
 
+        return descriptions
 
     @staticmethod
     def format_docs(docs):
         return "\n\n".join(doc.page_content for doc in docs)
-    
+
 
 if __name__ == "__main__":
     update_env_vars(".env")
@@ -199,7 +255,7 @@ if __name__ == "__main__":
     # video_url = "https://www.youtube.com/watch?v=lW7Mxj8KUJE&ab_channel=LinusTechTips"
     # query = "Why is google chrome slow based on the transcript?"
     # answer = rag.query_youtube_video(video_url, query)
-    
+
     # video_url = "https://www.youtube.com/watch?v=JDEc9Z_LI9I&ab_channel=SomeOrdinaryGamers"
     # query = "Why will the speaker won't buy EA games anymore?"
     # answer = rag.query_youtube_video(video_url, query)
