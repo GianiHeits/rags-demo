bda37082dc46959bde6a07aefbd3abdda88e6010
Giani <giani.statie@heits.digital>
2024-06-18 09:57:03 +0300

    Updated code to make image retrieval work "better"

--- chatbot_ui.py
+++ chatbot_ui.py
@@ -6,9 +6,6 @@ import gradio as gr
 from main import RAG, update_env_vars
 
 
-rag = RAG(streaming=False)
-rag.prepare_vectorstores_for_demo()
-
 def handler(message, history, task_type, additional_info):
     match task_type:
         case "Article Summary":
@@ -20,28 +17,52 @@ def handler(message, history, task_type, additional_info):
             response = rag.query_images(image_paths, message)
         case _:
             response = f"I currently do not support '{task_type}' functionality"
-    
+
     partial_message = ""
     for chunk in response:
+        time.sleep(0.02)
         partial_message += chunk
         yield partial_message
 
-demo = gr.ChatInterface(
-    handler,
-    additional_inputs=[
-        gr.Dropdown(
-            ["Article Summary", "YouTube Summary", "Image Summary"], label="Data Source", info="Choose what is the source for your RAG"
-        ),
-        gr.Textbox("https://heits.digital/articles/gpt3-overview", label="Additional Information", info="Choose what is the source for your RAG"),
-    ],
-    examples=[
-        ["Why was Elon Musk afraid to make ChatGPT public?", "Article Summary", "https://heits.digital/articles/gpt3-overview"],
-        ["Why is google chrome slow based on the transcript?", "YouTube Summary", "https://www.youtube.com/watch?v=lW7Mxj8KUJE&ab_channel=LinusTechTips"], 
-        ["Are there any pokemon cards? And if so, what colors are the pokemons?", "Image Summary", "data/images/"]
-    ],
-    cache_examples=True
-)
 
 if __name__ == "__main__":
     update_env_vars(".env")
+
+    rag = RAG(streaming=False)
+    rag.prepare_vectorstores_for_demo()
+
+    demo = gr.ChatInterface(
+        handler,
+        additional_inputs=[
+            gr.Dropdown(
+                ["Article Summary", "YouTube Summary", "Image Summary"],
+                label="Data Source",
+                info="Choose what is the source for your RAG",
+            ),
+            gr.Textbox(
+                "https://heits.digital/articles/gpt3-overview",
+                label="Additional Information",
+                info="Choose what is the source for your RAG",
+            ),
+        ],
+        examples=[
+            [
+                "Why was Elon Musk afraid to make ChatGPT public?",
+                "Article Summary",
+                "https://heits.digital/articles/gpt3-overview",
+            ],
+            [
+                "Why is google chrome slow based on the transcript?",
+                "YouTube Summary",
+                "https://www.youtube.com/watch?v=lW7Mxj8KUJE&ab_channel=LinusTechTips",
+            ],
+            [
+                "Are there any pokemon cards? And if so, what colors are the pokemons?",
+                "Image Summary",
+                "data/images/",
+            ],
+        ],
+        cache_examples=True,
+    )
+
     demo.queue().launch(share=True)
--- main.py
+++ main.py
@@ -12,7 +12,11 @@ from langchain_core.output_parsers import StrOutputParser, JsonOutputParser
 from langchain_core.runnables import RunnablePassthrough
 from langchain.output_parsers import PydanticOutputParser
 
-from transformers import BlipProcessor, BlipForConditionalGeneration, BlipForQuestionAnswering
+from transformers import (
+    BlipProcessor,
+    BlipForConditionalGeneration,
+    BlipForQuestionAnswering,
+)
 
 from src.youtube_source import get_transcript_from_url
 
@@ -25,10 +29,11 @@ class Questions(BaseModel):
     questions: List[str]
 
 
-def update_env_vars(env_file_path: str=None):
+def update_env_vars(env_file_path: str = None):
     if env_file_path:
         env_config = dotenv_values(env_file_path)
         os.environ = {**os.environ, **env_config}
+
 
 class RAG:
     def __init__(self, streaming=False):
@@ -37,33 +42,52 @@ class RAG:
 
         self.blip_model_type = "Salesforce/blip-image-captioning-large"
         self.blip_processor = BlipProcessor.from_pretrained(self.blip_model_type)
-        self.blip_model = BlipForConditionalGeneration.from_pretrained(self.blip_model_type)
-        
+        self.blip_model = BlipForConditionalGeneration.from_pretrained(
+            self.blip_model_type
+        )
+
         self.blip_qna_model_type = "Salesforce/blip-vqa-base"
-        self.blip_qna_processor = BlipProcessor.from_pretrained(self.blip_qna_model_type)
-        self.blip_qna_model = BlipForQuestionAnswering.from_pretrained(self.blip_qna_model_type)
+        self.blip_qna_processor = BlipProcessor.from_pretrained(
+            self.blip_qna_model_type
+        )
+        self.blip_qna_model = BlipForQuestionAnswering.from_pretrained(
+            self.blip_qna_model_type
+        )
 
         self.vectorstore_links_sources = None
         self.vectorstore_links = None
-        
+
         self.vectorstore_youtube_sources = None
         self.vectorstore_youtube = None
 
         self.vectorstore_images_sources = None
         self.vectorstore_images = None
 
-
     def prepare_vectorstores_for_demo(self):
-        self.vectorstore_links_sources = ["https://heits.digital/articles/gpt3-overview"]
-        self.vectorstore_links = self._get_vectorstore_from_links(self.vectorstore_links_sources)
+        self.vectorstore_links_sources = [
+            "https://heits.digital/articles/gpt3-overview"
+        ]
+        self.vectorstore_links = self._get_vectorstore_from_links(
+            self.vectorstore_links_sources
+        )
 
-        self.vectorstore_youtube_sources = "https://www.youtube.com/watch?v=lW7Mxj8KUJE&ab_channel=LinusTechTips"
-        self.vectorstore_youtube = self._get_vectorstore_from_youtube(self.vectorstore_youtube_sources)
+        self.vectorstore_youtube_sources = (
+            "https://www.youtube.com/watch?v=lW7Mxj8KUJE&ab_channel=LinusTechTips"
+        )
+        self.vectorstore_youtube = self._get_vectorstore_from_youtube(
+            self.vectorstore_youtube_sources
+        )
 
-        self.vectorstore_images_sources = [str(path) for path in Path("data/images/").glob("*")]
-        raw_images = [Image.open(img_path).convert('RGB') for img_path in self.vectorstore_images_sources]
-        self.vectorstore_images = self._get_extensive_vectorstore_from_images(raw_images, self.vectorstore_images_sources)
-
+        self.vectorstore_images_sources = [
+            str(path) for path in Path("data/images/").glob("*")
+        ]
+        raw_images = [
+            Image.open(img_path).convert("RGB")
+            for img_path in self.vectorstore_images_sources
+        ]
+        self.vectorstore_images = self._get_extensive_vectorstore_from_images(
+            raw_images, self.vectorstore_images_sources
+        )
 
     def query_links(self, links: List[str], query: str) -> str:
         if links != self.vectorstore_links_sources:
@@ -73,16 +97,18 @@ class RAG:
         rag_chain = self.get_rag_chain(self.vectorstore_links)
         return rag_chain.invoke(query)
 
-
     def query_images(self, image_path_list: List[str], query: str) -> str:
         if image_path_list != self.vectorstore_images_sources:
-            raw_images = [Image.open(img_path).convert('RGB') for img_path in image_path_list]
+            raw_images = [
+                Image.open(img_path).convert("RGB") for img_path in image_path_list
+            ]
             self.vectorstore_images_sources = raw_images
-            self.vectorstore_images = self._get_extensive_vectorstore_from_images(raw_images, image_path_list)
+            self.vectorstore_images = self._get_extensive_vectorstore_from_images(
+                raw_images, image_path_list
+            )
 
         rag_chain = self.get_rag_chain(self.vectorstore_images)
         return rag_chain.invoke(query)
-
 
     def query_youtube_video(self, video_url: str, query: str) -> str:
         if video_url != self.vectorstore_youtube_sources:
@@ -92,9 +118,10 @@ class RAG:
         rag_chain = self.get_rag_chain(self.vectorstore_youtube, 15)
         return rag_chain.invoke(query)
 
-
     def get_rag_chain(self, vectorstore, top_k=5):
-        retriever = vectorstore.as_retriever(search_type="similarity", search_kwargs={"k": top_k})
+        retriever = vectorstore.as_retriever(
+            search_type="similarity", search_kwargs={"k": top_k}
+        )
         prompt = hub.pull("rlm/rag-prompt")
 
         rag_chain = (
@@ -105,7 +132,6 @@ class RAG:
         )
 
         return rag_chain
-
 
     def _get_vectorstore_from_links(self, links):
         # Use this to ask GPT about content on websites
@@ -126,31 +152,44 @@ class RAG:
             )
             docs.extend(loader.load())
 
-        text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=200)
+        text_splitter = RecursiveCharacterTextSplitter(
+            chunk_size=1000, chunk_overlap=200
+        )
         splits = text_splitter.split_documents(docs)
         vectorstore = Chroma.from_documents(documents=splits, embedding=self.encoder)
 
         return vectorstore
-    
 
-    def _get_vectorstore_from_images(self, raw_images_list: List[Image.Image], raw_images_paths: List[str]=None, query: str=""):
+    def _get_vectorstore_from_images(
+        self,
+        raw_images_list: List[Image.Image],
+        raw_images_paths: List[str] = None,
+        query: str = "",
+    ):
         # Use this to ask GPT about content in images
         descriptions = self._extract_generic_image_info(raw_images_list)
-        descriptions_questions = self._extract_image_info_based_on_questions(raw_images_list, question=query)
+        descriptions_questions = self._extract_image_info_based_on_questions(
+            raw_images_list, question=query
+        )
 
         documents = []
         for i, [desc1, desc2] in enumerate(zip(descriptions, descriptions_questions)):
             source = raw_images_paths[i] if raw_images_paths != None else f"image_{i}"
-            description = "{" + f"file_name: '{source}', description_generic: '{desc1}', description_specific: '{desc2}'" + "}"
+            description = (
+                "{"
+                + f"file_name: '{source}', description_generic: '{desc1}', description_specific: '{desc2}'"
+                + "}"
+            )
             doc = Document(page_content=description, metadata={"source": source})
             documents.append(doc)
         print(documents)
-        
+
         vectorstore = Chroma.from_documents(documents=documents, embedding=self.encoder)
         return vectorstore
 
-
-    def _get_extensive_vectorstore_from_images(self, raw_images_list: List[Image.Image], raw_images_paths: List[str]=None):
+    def _get_extensive_vectorstore_from_images(
+        self, raw_images_list: List[Image.Image], raw_images_paths: List[str] = None
+    ):
         descriptions = self._extract_extensive_image_info(raw_images_list)
 
         documents = []
@@ -159,22 +198,21 @@ class RAG:
             description = "{" + f"file_name: '{source}', description: '{desc}'" + "}"
             doc = Document(page_content=description, metadata={"source": source})
             documents.append(doc)
-        
+
         vectorstore = Chroma.from_documents(documents=documents, embedding=self.encoder)
         return vectorstore
-
 
     def _get_vectorstore_from_youtube(self, video_url):
         # Use this to ask GPT about content in videos
         transcript = get_transcript_from_url(video_url)
-        
+
         documents = []
         for item in transcript:
             metadata = item.copy()
-            page_content = metadata.pop('text', None)
+            page_content = metadata.pop("text", None)
             doc = Document(page_content=page_content, metadata=metadata)
             documents.append(doc)
-        
+
         vectorstore = Chroma.from_documents(documents=documents, embedding=self.encoder)
         return vectorstore
 
@@ -183,9 +221,9 @@ class RAG:
         formatting = parser.get_format_instructions()
 
         prompt = ChatPromptTemplate.from_template(
-            "Pretend you are Sherlock Holmes and you ask as many questions in order to solve a mistery. What questions would ask about a image with the following description: {query}. {format_instructions}", 
-            partial_variables={"format_instructions": formatting}
-            )
+            "Pretend you are Sherlock Holmes and you ask as many questions in order to solve a mistery. What questions would ask about a image with the following description: {query}. Your questions need to start with `what`, `how` or `why`. {format_instructions}",
+            partial_variables={"format_instructions": formatting},
+        )
         chain = prompt | self.llm | JsonOutputParser()
         return chain.invoke(query)
 
@@ -207,7 +245,7 @@ class RAG:
             description = self.blip_processor.decode(out[0], skip_special_tokens=True)
             print(description)
             descriptions.append(description)
-        
+
         return descriptions
 
     def _extract_image_info_based_on_questions(self, raw_images: List, question=""):
@@ -217,21 +255,23 @@ class RAG:
         for image in raw_images:
             inputs = self.blip_qna_processor(image, question, return_tensors="pt")
             out = self.blip_qna_model.generate(**inputs, max_new_tokens=20)
-            description = self.blip_qna_processor.decode(out[0], skip_special_tokens=True)
+            description = self.blip_qna_processor.decode(
+                out[0], skip_special_tokens=True
+            )
             print(description)
             descriptions.append(description)
-        
+
         return descriptions
-    
+
     def _extract_extensive_image_info(self, raw_images: List):
         # get basic description of the images
         base_descriptions = self._extract_generic_image_info(raw_images)
-        
+
         extensive_descriptions = []
         for image, description in zip(raw_images, base_descriptions):
             # generate questions based on the basic description
             questions = self._get_questions_from_prompt(description)
-            print('Questions: ', questions["questions"])
+            print("Questions: ", questions["questions"])
 
             # get a more extensive description based on questions
             image_descriptions = []
@@ -239,8 +279,10 @@ class RAG:
                 print("Asking: ", question)
                 inputs = self.blip_qna_processor(image, question, return_tensors="pt")
                 out = self.blip_qna_model.generate(**inputs, max_new_tokens=20)
-                
-                answer = self.blip_qna_processor.decode(out[0], skip_special_tokens=True)
+
+                answer = self.blip_qna_processor.decode(
+                    out[0], skip_special_tokens=True
+                )
                 print("Answer: ", answer)
 
                 rephrased = self._rephrase_question_answer(question, answer)
@@ -248,14 +290,14 @@ class RAG:
                 print("Rephrased: ", rephrased)
 
                 image_descriptions.append(rephrased)
-            
+
             extensive_descriptions.append(" ".join(image_descriptions))
         return extensive_descriptions
 
     @staticmethod
     def format_docs(docs):
         return "\n\n".join(doc.page_content for doc in docs)
-    
+
 
 if __name__ == "__main__":
     update_env_vars(".env")
@@ -270,14 +312,14 @@ if __name__ == "__main__":
     # video_url = "https://www.youtube.com/watch?v=lW7Mxj8KUJE&ab_channel=LinusTechTips"
     # query = "Why is google chrome slow based on the transcript?"
     # answer = rag.query_youtube_video(video_url, query)
-    
+
     # video_url = "https://www.youtube.com/watch?v=JDEc9Z_LI9I&ab_channel=SomeOrdinaryGamers"
     # query = "Why will the speaker won't buy EA games anymore?"
     # answer = rag.query_youtube_video(video_url, query)
 
     # This is how you find out what's in a image
     image_paths = [str(path) for path in Path("data/images/").glob("*")]
-    raw_images = [Image.open(img_path).convert('RGB') for img_path in image_paths]
+    raw_images = [Image.open(img_path).convert("RGB") for img_path in image_paths]
     # query = "Are there any pokemon cards? And if so, what colors are the pokemons?."
     # answer = rag.query_images(image_paths, query)
     descriptions = rag._extract_extensive_image_info(raw_images)
